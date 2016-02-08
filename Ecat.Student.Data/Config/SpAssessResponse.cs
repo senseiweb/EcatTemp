33fb21f2d9f05d33c620b2f4eb2a147c3faea486
Sensei (JAL) <sensei31@icloud.com>
2016-02-08 11:24:52 -0600

    -- Forgotten files

--- Ecat.Student.Data/Config/SpAssessResponse.cs
+++ Ecat.Student.Data/Config/SpAssessResponse.cs
@@ -1,15 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
 using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Ecat.Shared.Core;
 using Ecat.Student.Core.Model;
 
-namespace Ecat.StudentOps.Data.Config
+namespace Ecat.Student.Data.Config
 {
     internal class ConfigSpAssessResponse : EntityTypeConfiguration<SpAssessResponse>
     {
+        public ConfigSpAssessResponse()
+        {
+            ToTable(MpTableNames.SpAssessResp);
 
+            HasRequired(p => p.Assessor)
+                .WithMany(p => p.AssessorSpResponses)
+                .HasForeignKey(p => p.AssessorId)
+                .WillCascadeOnDelete(false);
+
+            HasRequired(p => p.Assessee)
+               .WithMany(p => p.AssesseeSpResponses)
+               .HasForeignKey(p => p.AssesseeId)
+               .WillCascadeOnDelete(false);
+
+            HasRequired(p => p.InventoryItem)
+                .WithMany()
+                .HasForeignKey(p=> p.InventoryItemId)
+                .WillCascadeOnDelete(false);
+
+            Property(p => p.AssessorId)
+               .HasColumnAnnotation(IndexAnnotation.AnnotationName,
+                   new IndexAnnotation(new IndexAttribute("IX_UniqueSpResponse", 1) { IsUnique = true }));
+
+            Property(p => p.AssesseeId)
+                .HasColumnAnnotation(IndexAnnotation.AnnotationName,
+                    new IndexAnnotation(new IndexAttribute("IX_UniqueSpResponse", 2) { IsUnique = true }));
+
+            Property(p => p.InventoryItemId)
+               .HasColumnAnnotation(IndexAnnotation.AnnotationName,
+                   new IndexAnnotation(new IndexAttribute("IX_UniqueSpResponse", 3) { IsUnique = true }));
+        }
     }
 }
