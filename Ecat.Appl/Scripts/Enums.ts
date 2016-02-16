7f6cd3b5726e58ef5093394a62a8f656d3518a9d
Sensei (JAL) <sensei31@icloud.com>
2016-02-16 10:24:03 -0600

    -- Transfer to work computer

--- Ecat.Appl/Scripts/Enums.ts
+++ Ecat.Appl/Scripts/Enums.ts
@@ -1,26 +1,4 @@
 module Ecat.Shared.Model {
-	export const enum EdLevel {
-		None = 0,
-		Chief = 2,
-		Senior = 4,
-		Ncoa = 8,
-		Airman = 16,
-		Instructor = 32
-	}
-	export const enum AcademyBase {
-		None = 0,
-		Gunter = 2,
-		Keesler = 4,
-		Lackland = 8,
-		Peterson = 16,
-		Tyndall = 32,
-		Sheppard = 64,
-		McGheeTyson = 128,
-		Elmendorf = 256,
-		Hickam = 512,
-		Kadena = 1024,
-		Kisling = 2048
-	}
 	export const enum RoleMap {
 		Unknown = 0,
 		SysAdmin = 1,
