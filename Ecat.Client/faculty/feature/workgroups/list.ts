7f5788249dde200e7200c47bc2f49872dfb31465
Johnny Lockhart <sensei31@icloud.com>
2016-02-29 19:33:12 -0600

    Started List view restructure

--- Ecat.Client/faculty/feature/workgroups/list.ts
+++ Ecat.Client/faculty/feature/workgroups/list.ts
@@ -1,10 +1,29 @@
-﻿export default class EcFacultyWgList {
+﻿import * as _mp from 'core/common/mapStrings'
+
+export default class EcFacultyWgList {
     static controllerId = 'app.faculty.wkgrp.list';
     static $inject = [];
-
+    
+    private mp = _mp.MpSpStatus;
+    private course: ecat.entity.ICourse;
+    
     constructor() {
         
     }
-
-
+    
+    private goToAssess(wg: ecat.entity.IWorkGroup): void {
+        
+    }
+    
+    private goToPublish(wg: ecat.entity.IWorkGroup): void {
+        //TODO: Check if all work is done, if not error;
+    }
+    
+    private goToResults(wg: ecat.entity.IWorkGroup): void {
+        
+    }
+    
+    private viewStatus(wg: ecat.entity.IWorkGroup): void {
+        
+    }
 }
